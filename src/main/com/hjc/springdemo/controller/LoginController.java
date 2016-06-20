54ff2ed5d5b6963379366a24e90a6ce97d9a19b0
SSMTest <lingshao2010@126.com>
2016-06-20 18:23:32 +0800

    问题无法映射对应controller

--- src/main/com/hjc/springdemo/controller/LoginController.java
+++ src/main/com/hjc/springdemo/controller/LoginController.java
@@ -3,12 +3,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value="/SpringDemo/rest/page")
+@RequestMapping(value="/rest/page")
 public class LoginController {
 	
 	@RequestMapping(value="/login")
 	public ModelAndView toIndex(){
 		ModelAndView mv = this.getModelAndView();
-		mv.addObject("/rest/index");
+		System.out.println("sadfa");
+		mv.addObject("/index");
+		return mv;
 	}
 }
