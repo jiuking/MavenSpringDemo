822ff3c9d6180f8c6db9b776e69be40a89dcc4a1
SSMTest <lingshao2010@126.com>
2016-06-20 18:35:00 +0800

    修改结构

--- src/main/java/com/hjc/springdemo/controller/LoginController.java
+++ src/main/java/com/hjc/springdemo/controller/LoginController.java
@@ -1,14 +1,15 @@
+package com.hjc.springdemo.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 @Controller
 @RequestMapping(value="/rest/page")
 public class LoginController {
-	
+
 	@RequestMapping(value="/login")
 	public ModelAndView toIndex(){
-		ModelAndView mv = this.getModelAndView();
+		ModelAndView mv = new ModelAndView();
 		System.out.println("sadfa");
 		mv.addObject("/index");
 		return mv;
