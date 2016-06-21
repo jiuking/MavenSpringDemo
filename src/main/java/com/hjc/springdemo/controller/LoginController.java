60ab7bece538ae3c22f889a777ed6bf41fcc81f4
hjc <hjc@sc.10086.cn.defaultbadlist>
2016-06-21 19:13:31 +0800

    冲突

--- src/main/java/com/hjc/springdemo/controller/LoginController.java
+++ src/main/java/com/hjc/springdemo/controller/LoginController.java
@@ -3,15 +3,22 @@ package com.hjc.springdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 @RequestMapping(value="/rest/page")
 public class LoginController {
 
 	@RequestMapping(value="/login")
+	public ModelAndView toLogin(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/page/login");
+		return mv;
+	}
+	
+	@RequestMapping(value="/index")
 	public ModelAndView toIndex(){
 		ModelAndView mv = new ModelAndView();
-		System.out.println("sadfa");
-		mv.addObject("/index");
+		mv.setViewName("/page/401");
 		return mv;
 	}
 }
