55562a12ae4c54f0a80946efb16823aa8ed37581
hjc <hjc@192.168.1.108>
2016-06-22 19:37:50 +0800

    realm

--- src/main/java/com/hjc/springdemo/security/MonitorRealm.java
+++ src/main/java/com/hjc/springdemo/security/MonitorRealm.java
@@ -15,6 +15,8 @@ public class MonitorRealm extends AuthorizingRealm {
 		super();
 	}
 	
+	//Authorization：授权，即权限验证，验证某个已认证的用户是否拥有某个权限；即判断用户是否能做事情，
+	//常见的如：验证某个用户是否拥有某个角色。或者细粒度的验证某个用户对某个资源是否具有某个权限；
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
 		// TODO Auto-generated method stub
@@ -22,6 +24,7 @@ public class MonitorRealm extends AuthorizingRealm {
 		return authorizationInfo;
 	}
 
+//	Authentication：身份认证/登录，验证用户是不是拥有相应的身份；
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken arg0) throws AuthenticationException {
 		// TODO Auto-generated method stub
